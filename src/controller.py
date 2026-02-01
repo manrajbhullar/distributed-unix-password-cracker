3cb7f68d6f80b57397b6e25c50a7053f041cdba6
manrajbhullar <manraj-bhullar@outlook.com>
2026-02-01 03:17:51 -0800

    Controller program structure

---
+++ src/controller.py
@@ -0,0 +1,28 @@
+import argparse
+import socket 
+
+HOST = "127.0.0.1"
+PORT = 8000
+
+
+def parse_arguments():
+    pass
+
+
+def handle_arguments():
+    pass
+
+
+def parse_shadow():
+    pass
+
+
+def listen():
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
