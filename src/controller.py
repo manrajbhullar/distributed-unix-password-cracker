ab11aeb6dd38a97f3fadc1b5f4cf79ae824e5fdc
manrajbhullar <manraj-bhullar@outlook.com>
2026-01-31 22:49:34 -0800

    Initial FSM flow and parse_arguments implemented

--- src/controller.py
+++ src/controller.py
@@ -1,16 +1,78 @@
 import argparse
-import socket 
+from dataclasses import dataclass
+from enum import Enum, auto
+import sys
 
-HOST = "127.0.0.1"
-PORT = 8000
+
+class State(Enum):
+    PARSE_ARGS = auto()
+    HANDLE_ARGS = auto()
+    EXIT = auto()
+    ERROR = auto()
 
 
-def parse_arguments():
+@dataclass
+class Context:
+    filename: str | None = None
+    username: str | None = None
+    port: int | None = None
+
+
+def parse_arguments(ctx: Context) -> State:
+    parser = argparse.ArgumentParser(
+        prog="UNIX Password Cracker Controller",
+        description="This is a control server that manages distributed password cracking.",
+        epilog=""
+    )
+    
+
+    parser.add_argument(
+        "-f", "--filename",
+        type=str, 
+        help="Name of shadow file", 
+        required=True
+    )
+
+    parser.add_argument(
+        "-u", "--username",
+        type=str, 
+        help="Username whose password being cracked", 
+        required=True
+    )
+
+    parser.add_argument(
+        "-p", "--port",
+        type=int, 
+        help="Port number control server runs on", 
+        required=True
+    )
+
+    args = parser.parse_args()
+
+    ctx.filename = args.filename
+    ctx.username = args.username
+    ctx.port = args.port
+    
+    return State.HANDLE_ARGS
+    
+
+
+def handle_arguments(ctx: Context) -> State:
+    print(ctx.filename)
+    print(ctx.username)
+    print(ctx.port)
+
+    return State.EXIT
+
+
+def exit_program(ctx):
+    print("Exiting program")
+    sys.exit(0)
+
+
+def error(ctx: Context) -> State:
     pass
 
-
-def handle_arguments():
-    pass
 
 
 def parse_shadow():
@@ -22,7 +84,19 @@ def listen():
 
 
 def main():
-    pass
+    ctx = Context()
+    state = State.PARSE_ARGS
+
+    handlers = {
+        State.PARSE_ARGS: parse_arguments,
+        State.HANDLE_ARGS: handle_arguments,
+        State.EXIT: exit_program
+    }
+
+    while state != State.EXIT:
+        state = handlers[state](ctx)
+    handlers[State.EXIT](ctx)
+    
 
 if __name__ == "__main__":
     main()
